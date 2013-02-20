b4cedfe29586aa0dfcbcc4754ce21499a300b6d2
James Rickard <james@frodosghost.com>
2013-02-21 08:26:42 +1100

    Updated test to include correct logger file name.

--- Tests/Data/AtomLoggerXmlDataTest.php
+++ Tests/Data/AtomLoggerXmlDataTest.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Test the LoggerXmlData
+ * Test the AtomLoggerXml
  */
 
 namespace Manhattan\LogBundle\Tests\Data;
 
-use Manhattan\LogBundle\Data\LoggerXmlData;
+use Manhattan\LogBundle\Data\AtomLoggerXmlData;
 
 class AtomLoggerXmlDataTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,15 +15,15 @@ class AtomLoggerXmlDataTest extends \PHPUnit_Framework_TestCase
      */
     public function testConstruct()
     {
-        $xml_data = new LoggerXmlData(array('foo' => array(
+        $xml_data = new AtomLoggerXmlData(array('foo' => array(
             'bar' => 'foo')
         ));
-        $this->assertInstanceOf('Manhattan\LogBundle\Data\LoggerXmlData', $xml_data);
+        $this->assertInstanceOf('Manhattan\LogBundle\Data\AtomLoggerXmlData', $xml_data);
     }
 
     public function testSingleRootNodeException()
     {
-        $xml_data = new LoggerXmlData(array('foo' => 'bar', 'bar' => 'foo'));
+        $xml_data = new AtomLoggerXmlData(array('foo' => 'bar', 'bar' => 'foo'));
 
         $this->setExpectedException('Restful\Exception\DataException');
         $xml_data->validate();
@@ -31,7 +31,7 @@ class AtomLoggerXmlDataTest extends \PHPUnit_Framework_TestCase
 
     public function testSingleGetNodeVariable()
     {
-        $xml_data = new LoggerXmlData(array('foo' => 'bar'));
+        $xml_data = new AtomLoggerXmlData(array('foo' => 'bar'));
 
         $this->setExpectedException('InvalidArgumentException');
         $xml_data->validate();
@@ -39,7 +39,7 @@ class AtomLoggerXmlDataTest extends \PHPUnit_Framework_TestCase
 
     public function testNonMatchingArray()
     {
-        $xml_data = new LoggerXmlData(array('foo' => array(
+        $xml_data = new AtomLoggerXmlData(array('foo' => array(
             'foo' => 'bar'
         )));
 
@@ -49,7 +49,7 @@ class AtomLoggerXmlDataTest extends \PHPUnit_Framework_TestCase
 
     public function testValidation()
     {
-        $xml_data = new LoggerXmlData(array('foo' => array(
+        $xml_data = new AtomLoggerXmlData(array('foo' => array(
             'Message' => 'bar'
         )));
 
