68221da95fb1539004ab4872c60af6c7ac2a1e50
James Rickard <james@frodosghost.com>
2013-02-21 12:07:34 +1100

    Updated Request class to test the formatter which is called.

--- Connection/Request.php
+++ Connection/Request.php
@@ -3,27 +3,60 @@
 
 namespace Manhattan\LogBundle\Connection;
 
+use Restful\Formatter\XmlFormatter;
 use Manhattan\LogBundle\Connection\Client;
+use Manhattan\LogBundle\Data\AtomLoggerXmlData;
+
+use Restful\Exception\DataException;
+use Manhattan\LogBundle\Exception\ConfigurationException;
 
 /**
+ * Setup the Request for sending to AtomLogger
+ *
  * @author James Rickard <james@frodosghost.com>
  */
 class Request
 {
 
     /**
-     * @var Manhattan\LogBundle\Connection\Client
+     * @var Restful\Formatter\XmlFormatter
      */
-    private $client;
+    private $formatter;
 
     /**
-     * @var array
+     * @var Restful\Data\AbstractData
      */
     private $data;
 
-    public function __construct(Client $client)
+    public function __construct(XmlFormatter $formatter, AtomLoggerXmlData $data)
     {
-        $this->client = $client;
+        $this->formatter = $formatter;
+        $this->data = $data;
+    }
+
+    public function setData(array $data)
+    {
+        $this->data->append(array('root-node' => $data));
+
+        return $this;
+    }
+
+    /**
+     * Calls the Formatter with the data passed into the Request
+     * 
+     * @return string
+     */
+    public function formatData()
+    {
+        $this->formatter->addData($this->data);
+
+        try {
+            $formatted = $this->formatter->format();
+        } catch (DataException $e) {
+            throw new ConfigurationException('The provided data has been incorrectly formatted.');
+        }
+
+        return $formatted;
     }
 
 }
--- Tests/Connection/RequestTest.php
+++ Tests/Connection/RequestTest.php
@@ -15,12 +15,40 @@ class RequestTest extends \PHPUnit_Framework_TestCase
      */
     public function testConstruct()
     {
-        $mock_client = $this->getMockBuilder('Manhattan\LogBundle\Connection\Client')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $mock_formatter = $this->getMock('Restful\Formatter\XmlFormatter');
+        $mock_data = $this->getMock('Manhattan\LogBundle\Data\AtomLoggerXmlData');
 
-        $handler = new Request($mock_client);
-        $this->assertInstanceOf('Manhattan\LogBundle\Connection\Request', $handler);
+        $request = new Request($mock_formatter, $mock_data);
+
+        $this->assertInstanceOf('Manhattan\LogBundle\Connection\Request', $request);
     }
 
+    public function testFormatDataException()
+    {
+        $mock_formatter = $this->getMock('Restful\Formatter\XmlFormatter');
+        $mock_data = $this->getMock('Manhattan\LogBundle\Data\AtomLoggerXmlData');
+
+        $mock_formatter->expects($this->any())
+             ->method('format')
+             ->will($this->throwException(new \Restful\Exception\DataException));
+
+        $request = new Request($mock_formatter, $mock_data);
+
+        $this->setExpectedException('Manhattan\LogBundle\Exception\ConfigurationException');
+        $request->formatData();
+    }
+
+    public function testFormatData()
+    {
+        $mock_formatter = $this->getMock('Restful\Formatter\XmlFormatter');
+        $mock_data = $this->getMock('Manhattan\LogBundle\Data\AtomLoggerXmlData');
+
+        $mock_formatter->expects($this->any())
+             ->method('format')
+             ->will($this->returnValue('<foo></foo>'));
+
+        $request = new Request($mock_formatter, $mock_data);
+
+        $this->assertEquals('<foo></foo>', $request->formatData());
+    }
 }
